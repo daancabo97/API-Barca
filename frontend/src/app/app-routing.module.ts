6c50517b1a61b3ebf3c9467deab4a144fcf0aa88
daancabo97 <danielcaicedo285@gmail.com>
2025-04-23 23:48:27 -0500

    frontend

---
+++ frontend/src/app/app-routing.module.ts
@@ -0,0 +1,27 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { LoginComponent } from './pages/login/login.component';
+import { RegistroComponent } from './pages/registro/registro.component';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { JugadoresComponent } from './pages/jugadores/jugadores.component';
+import { PartidosComponent } from './pages/partidos/partidos.component';
+import { EstadisticasComponent } from './pages/estadisticas/estadisticas.component';
+import { AuthGuard } from './guards/auth.guard';
+
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'registro', component: RegistroComponent },
+  { path: 'dashboard', component: DashboardComponent, canActivate: [AuthGuard] },
+  { path: 'jugadores', component: JugadoresComponent, canActivate: [AuthGuard] },
+  { path: 'partidos', component: PartidosComponent, canActivate: [AuthGuard] },
+  { path: 'estadisticas', component: EstadisticasComponent, canActivate: [AuthGuard] },
+  { path: '**', redirectTo: '' }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
---
+++ frontend/src/app/app.module.ts
@@ -0,0 +1,36 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule, provideClientHydration, withEventReplay } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HomeComponent } from './pages/home/home.component';
+import { LoginComponent } from './pages/login/login.component';
+import { RegistroComponent } from './pages/registro/registro.component';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { JugadoresComponent } from './pages/jugadores/jugadores.component';
+import { PartidosComponent } from './pages/partidos/partidos.component';
+import { EstadisticasComponent } from './pages/estadisticas/estadisticas.component';
+import { NavbarComponent } from './shared/navbar/navbar.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    LoginComponent,
+    RegistroComponent,
+    DashboardComponent,
+    JugadoresComponent,
+    PartidosComponent,
+    EstadisticasComponent,
+    NavbarComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [
+    provideClientHydration(withEventReplay())
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
